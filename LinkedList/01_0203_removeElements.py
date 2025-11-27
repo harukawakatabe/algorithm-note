b062d648e7fc31b61d54b957ede93d237c948081
susy82790 <susy82790@hotmail.com>
2025-11-27 23:40:27 +0800

    linkedlist03

--- LinkedList/01_0203_removeElements.py
+++ LinkedList/01_0203_removeElements.py
@@ -1,8 +1,12 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+        
+from typing import Optional
+
+
 class Solution:
     def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
         dummy_head = ListNode(next=head)
--- LinkedList/03_0206_reverseList.py
+++ LinkedList/03_0206_reverseList.py
@@ -14,3 +14,49 @@ https://leetcode.cn/problems/reverse-linked-list/description/
 
 """
 
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        cur = head
+        pre = None
+        # 终止条件
+        while cur:
+            # 存
+            tmp = cur.next
+            # 反转
+            cur.next = pre
+            # 移动
+            pre = cur
+            cur = tmp
+        return pre
+
+
+
+
+
+
+# # 递归写法
+# class Solution(object):
+#     def reverse(self,cur,pre):
+#         # 先判断结束
+#         if cur == None:
+#             return pre
+#         tmp = cur.next
+#         cur.next = pre
+#         return self.reverse(tmp,cur) # 需要将处理完的值return出去
+
+
+#     def reverseList(self, head):
+#         """
+#         :type head: Optional[ListNode]
+#         :rtype: Optional[ListNode]
+#         """
+#         return self.reverse(head,None)
